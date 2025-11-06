dbeca8ac4364b6214a51fe04dda9e38121a85212
Vanja <vanjabrkans@gmail.com>
2025-11-06 19:04:26 +0100

    added contact page

---
+++ script.js
@@ -0,0 +1,9 @@
+ 
+// Select the button using its ID
+const myButton = document.getElementById("myButton");
+
+// Add a click event listener to the button
+myButton.addEventListener("click", function() {
+  // navigate to the contact page when clicked
+  window.location.href = './pages/contact.html';
+});
